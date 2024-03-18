2851133843f58a562e43c06116953574362346e0
Roman Hera <romahera6@gmail.com>
2024-03-18 21:36:57 +0200

    Add files via upload

---
+++ CharDemo.java
@@ -0,0 +1,15 @@
+
+/*
+Демонстрація використання типу даних char
+*/
+public class CharDemo {
+
+    public static void main(String[] args) {
+        char ch1, ch2;
+        ch1 = 88; //код для Х
+        ch2 = 'Y';
+        System.out.print("ch1 and ch2: ");
+        System.out.println(ch2 + " " + ch2);
+        
+    }
+}
