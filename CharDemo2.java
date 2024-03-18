586566dfbeae2c51a73bbc2d9600dc5bbd72c9dc
Roman Hera <romahera6@gmail.com>
2024-03-18 21:49:13 +0200

    Add files via upload

---
+++ CharDemo2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chardemo2;
+
+/**
+ *
+ * @author romah
+ */
+/*
+Змінна char ведуть себе подібно цілим числам
+*/
+public class CharDemo2 {
+
+    public static void main(String[] args) {
+        char ch1;
+        ch1 = 'X';
+        System.out.println("ch1 heve " + ch1);
+        ch1++; // інкримітує ch1
+        System.out.println("ch1 now have " + ch1);
+    }
+}
