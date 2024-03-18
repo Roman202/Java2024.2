c3717ee7ef697c7d87e4196cb5d0bf0e3f985816
Roman Hera <romahera6@gmail.com>
2024-03-18 21:17:48 +0200

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.area1;
+
+/**
+ *
+ * @author romah
+ */
+
+/*
+Площа кругу
+*/
+ public class Area {
+
+    public static void main(String[] args) {
+        double pi, r, a;
+        r = 10.8;     // Радіус
+        pi = 3.1416;  // Число пі
+        a = pi * r * r;
+        System.out.println("Area " + a);
+    }
+}
+
+  
