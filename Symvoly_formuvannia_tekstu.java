f0ea0d5a95a173686ebb940c3bdd4429d83c4816
Roman Hera <romahera6@gmail.com>
2024-03-25 19:41:29 +0200

    Add files via upload

---
+++ Symvoly_formuvannia_tekstu.java
@@ -0,0 +1,12 @@
+class Symvoly_formuvannia_tekstu {
+
+    public static void main(String[] args) {
+
+        // для переміщення (початок нового рядка) використовується код \n
+        System.out.println("Перший рядок\nДругий рядок");
+
+        // знак табуляції помагає оформити вихідний текст в колонки. Він виводиться з допомогою кода \t
+        System.out.println("A\tB\tH");
+        System.out.println("D\tE\tF");
+    }
+}
